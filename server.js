1a6fd55ad1baa339c7ae2887879f5a9af76d8f31
Tripathi, Amit (ITCDEC) - KLM <Amit.Tripathi@klm.com>
2020-05-01 22:15:03 +0200

    Changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 80;
 const fruits = require('./fruits');
 
 app.use(bodyParser.json());
